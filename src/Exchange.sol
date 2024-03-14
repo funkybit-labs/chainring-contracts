277ce53343eb42a1ad0f3d18c3d9d67669705088
Brendan Flood <162615268+bflood366@users.noreply.github.com>
2024-03-14 08:48:06 -0400

    upgradable contract changes (#1)

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "./common/Constants.sol";
+import "./interfaces/IVersion.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Initializable} from "openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-contract Exchange {
+contract Exchange is UUPSUpgradeable, OwnableUpgradeable, IVersion {
     mapping(address => mapping(address => uint256)) public balances;
-    address public owner = msg.sender;
 
     event DepositCreated();
     event WithdrawalCreated(uint256);
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function getVersion() external pure returns (uint64) {
+        return VERSION;
+    }
 
     function deposit(address _token, uint256 _amount) external {
         IERC20 erc20 = IERC20(_token);
